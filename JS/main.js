3961b69c6f1de4772445bc342e450d1a7b56725a
Vaibhav Kothi <170010116016@adit.ac.in>
2020-03-29 22:01:26 +0530

    Minor Manifest Tweeks

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,27 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  deferredPrompt = e;
+  showInstallPromotion();
+});
+window.addEventListener('beforeinstallprompt', (e) => {
+  deferredPrompt = e;
+  addBtn.style.display = 'block';
+  addBtn.addEventListener('click', (e) => {
+    addBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
+
 function loader(){
 	const flag = 0;
 	if(flag === 0){
